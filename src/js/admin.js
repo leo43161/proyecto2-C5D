93d916a204cbb46ad6ca9f9af3c8bd6f03e4200c
Lucas <lucasismaelgutierrez@gmail.com>
2020-07-18 17:53:44 -0300

    Se creo la rama DEV

--- src/js/admin.js
+++ src/js/admin.js
@@ -7,35 +7,3 @@ import '../css/style.css'
 
 
 
-//Validacion del formulario
-
-window.revisar = function(input) {
-    if (input.value == "") {
-        input.className = "form-control is-invalid";
-        return false;
-    } else {
-        input.className = "form-control is-valid";
-        return true;
-    }
-};
-
-window.revisarNumero = function (input) {
-    if (isNaN(input.value) || input.value == "") {
-        input.className = "form-control is-invalid"
-        return false;
-
-    } else {
-        input.className = "form-control is-valid";
-        return true;
-    }
-}
-
-window.revisarLongitud = function (input) {
-    if (input.value != "" && input.value.length >= 10) {
-        input.className = "form-control is-valid"
-        return true;
-    } else {
-        input.className = "form-control is-invalid"
-        return false;
-    }
-}
