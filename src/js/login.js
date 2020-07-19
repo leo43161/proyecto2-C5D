53bff9c8c71b1c97e3ce0f07345002230cde331d
Leonardo Palavecino <leo43161301gmail.com>
2020-07-19 00:38:53 -0300

    Parcialmente validado

--- src/js/login.js
+++ src/js/login.js
@@ -3,3 +3,52 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap'
 
 import '../css/style.css'
+
+function inputInvalido(input){
+    input.className = "form-control is-invalid";
+}
+
+function inputValido(input){
+    input.className = "form-control is-valid";
+}
+
+/* window.validarUsuario = function(value){
+    value.preventDefault();
+    let correo = document.getElementById("correo");
+    let contraseña = document.getElementById("contraseña");
+    if(contraseña.value == ""){
+        inputInvalido(contraseña);
+    }else if (correo.value == ""){
+        inputInvalido(correo);
+        contraseña.value = "";
+    }else if(contraseña.value == "" && correo.value == ""){
+        inputInvalido(contraseña);
+        inputInvalido(correo);
+    }
+} */
+
+window.validarCorreo = function(correo){
+    let expresion = /\w+@\w+\.[a-z]/;
+    if(correo.value != ""){
+        if(expresion.test(correo.value)){
+            inputValido(correo);
+            console.log("esta bien el correo");
+        }else{
+            inputInvalido(correo);
+        }
+    }else{
+        inputInvalido(correo);
+    }
+} 
+
+window.validarContraseña = function(contraseña){
+    if(contraseña.value != ""){
+        if(contraseña.value.length > 7){
+            inputValido(contraseña);
+        }else{
+            inputInvalido(contraseña);
+        }
+    }else{
+        inputInvalido(contraseña);
+    }
+}
